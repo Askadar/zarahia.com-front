a808c831a37dcee78f273141a1bfc4cb25f67ed1
Azriel <askadar.ru@gmail.com>
2017-07-26 10:51:35 +0300

    Configuration

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+	entry: path.join(__dirname, 'src', 'index'),
+	output: {
+		path: path.join(__dirname, '../zarahia.com/public'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			loader: 'babel-loader',
+			include: path.join(__dirname, '')
+		},]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	}
+};
