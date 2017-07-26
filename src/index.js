0d50e7f5e98792161ce4b48372059f6b4cc6d4de
Azriel <askadar.ru@gmail.com>
2017-07-26 10:51:41 +0300

    Basic app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { Provider } from 'mobx-react';
+// import AppState from './AppState';
+import App from './App';
+
+// const appState = new AppState();
+
+render(
+	<App/>,
+	document.getElementById('root')
+);
